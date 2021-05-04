8f9c09a8eec2ee9a2930c533cc50f6ff8199aec2
Igor(Exployt) <igoreusttaquio@gmail.com>
2021-05-04 16:05:25 +0000

    Added a new rout in api

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 
-from api.model.beer import Beer
+from api.model.beer import Beer, BeerList, NewBeer
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(Beer, '/beer/<int:id>')
+api.add_resource(Beer, '/beer/<int:_id>')
+api.add_resource(NewBeer, '/new-beer')
+api.add_resource(BeerList, '/beers')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
